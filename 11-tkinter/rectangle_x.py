4f7b738b749157017e9851bfd9181c861e3f3fd7
Matúš Turiak <turiakmatus@gmail.com>
2023-11-08 11:09:17 +0100

    add 11_tkinter and 11_du

---
+++ 11-tkinter/rectangle_x.py
@@ -0,0 +1,34 @@
+import tkinter
+
+canvas = tkinter.Canvas()
+canvas.pack()
+
+x = 10
+y = 10
+d = 20
+farba = "blue"
+canvas.create_rectangle(x, y, x+d, y+d, fil=farba)
+canvas.create_rectangle(x+d, y+d, x+2*d, y+2*d, fill=farba)
+canvas.create_rectangle(x+2*d, y+2*d, x+3*d, y+3*d, fill=farba)
+canvas.create_rectangle(x+3*d, y+2*d, x+4*d, y+d, fill=farba)
+canvas.create_rectangle(x+4*d, y+d, x+5*d, y, fil=farba)
+canvas.create_rectangle(x+d, y+4*d, x+2*d, y+3*d, fill=farba)
+canvas.create_rectangle(x, y+5*d, x+d, y+4*d, fill=farba)
+canvas.create_rectangle(x+3*d, y+3*d, x+4*d, y+4*d, fill=farba)
+canvas.create_rectangle(x+4*d, y+4*d, x+5*d, y+5*d, fill=farba)
+
+x = 6 * d
+y = 10
+farba = "red"
+canvas.create_rectangle(x, y, x+d, y+d, fil=farba)
+canvas.create_rectangle(x+d, y+d, x+2*d, y+2*d, fill=farba)
+canvas.create_rectangle(x+2*d, y+2*d, x+3*d, y+3*d, fill=farba)
+canvas.create_rectangle(x+3*d, y+2*d, x+4*d, y+d, fill=farba)
+canvas.create_rectangle(x+4*d, y+d, x+5*d, y, fil=farba)
+canvas.create_rectangle(x+d, y+4*d, x+2*d, y+3*d, fill=farba)
+canvas.create_rectangle(x, y+5*d, x+d, y+4*d, fill=farba)
+canvas.create_rectangle(x+3*d, y+3*d, x+4*d, y+4*d, fill=farba)
+canvas.create_rectangle(x+4*d, y+4*d, x+5*d, y+5*d, fill=farba)
+
+
+canvas.mainloop()
